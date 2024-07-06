fad592e354ccb7299754757612c8e78f2c129ba9
Raman Kumar <150523189+ramanjh@users.noreply.github.com>
2024-07-06 17:15:52 +0530

    .

---
+++ Episode-07 React Router Dynamic Routing/src/Header.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import {LOGO_URL} from "../utils.js/constants";
+import { Link } from "react-router-dom";
+import Body from "./Body";
+
+const Header=()=>{
+  const [btnNameReact ,  setBtnNameReact] = useState("Login");
+    return(
+      <div className="header">
+      <div className="logo-container">
+       <img className="logo"
+       src={LOGO_URL}/>
+       </div>
+  <div className="nav-items">
+   <ul>
+    <li><Link to = "/">Home</Link></li>
+    <li><Link  to ="/About">About Us</Link></li>
+    <li><Link to ="/Contact">Contact Us</Link></li>
+    <li><Link  to ="/Cart">Cart</Link></li>
+   
+    <button 
+     className="login"
+     onClick={()=>{
+       btnNameReact === "Login"
+       ? setBtnNameReact("Logout")
+       : setBtnNameReact("Login");
+
+     }}>{btnNameReact}</button>
+
+   </ul>
+   </div>
+  </div>
+     
+  );
+  };
+  
+  export default Header;
---
+++ Episode-07 React Router Dynamic Routing/src/RestaurantMenu.js
@@ -0,0 +1,54 @@
+import {useState , useEffect} from "react";
+import Shimmer from "./Shimmer";
+import {Menu_api} from "../utils.js/constants";
+import {useParams } from "react-router-dom";
+
+const RestaurantMenu = ()=> {
+
+    const[resInfo, setresInfo] = useState(null);
+    
+  const { resId } = useParams();
+
+useEffect(()=> {
+      fetchMenu();
+    },[]);
+
+    const fetchMenu = async ()=> {
+        const data = await fetch(Menu_api+resId);
+        
+        const json = await data.json();
+        
+    console.log(json);
+
+        setresInfo(json.data);
+    };
+
+    if ( resInfo === null) return <Shimmer/> ;
+
+const {name,cuisines,costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
+    
+
+    
+    const { itemCards } =  resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+    console.log(itemCards);
+
+    return  (
+        <div className="Menu">
+            <h1>{name}</h1>
+            <p>
+                {cuisines.join( ",")} - {costForTwoMessage}
+            </p>
+            <h2>Menu</h2>
+            <ul>
+                {itemCards.map(item=> <li key = {item.card.info.id}>{item.card.info.name}  -  {"Rs."} {item.card.info.price/100}</li>)}           
+            </ul>
+
+        </div>
+    )
+}
+
+    
+
+
+export default RestaurantMenu ;
