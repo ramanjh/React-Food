55d1c610275ba35eff30a5113e97f2e571e6c1aa
Raman Kumar <ramanjha93@gmail.com>
2024-07-24 00:59:53 +0530

    .

---
+++ Episode-13/src/utils/constants.js
@@ -0,0 +1,9 @@
+
+export  const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+
+export const LOGO_URL="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const rescard_URL = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const Menu_api = "https:thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
